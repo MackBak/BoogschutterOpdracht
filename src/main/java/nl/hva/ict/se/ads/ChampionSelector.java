5084ff4e560f0eec2a0bedf37b6afa8b866741f4
MackBak <154362357+MackBak@users.noreply.github.com>
2024-07-09 18:27:49 +0200

    implement: bubble sort & insertion sort with conclusion

--- src/main/java/nl/hva/ict/se/ads/ChampionSelector.java
+++ src/main/java/nl/hva/ict/se/ads/ChampionSelector.java
@@ -1,7 +1,7 @@
 package nl.hva.ict.se.ads;
 
+import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -11,17 +11,49 @@ public class ChampionSelector {
     /**
      * This method uses either selection sort or insertion sort for sorting the archers.
      */
-    public static List<Archer> basicSort(List<Archer> archers, Comparator<Archer> scoringScheme) {
-        // TODO implement a basic sort method of your choice
-        return archers;
-    }
 
+    public static List<Archer> basicSort(List<Archer> archers, Comparator<Archer> scoringScheme) {
+
+       // BUBBLE SORT:
+
+//        int n = archers.size(); // Size of the archer list is stored in variable n.
+//        boolean swapped;        // Initliazing a boolean to check if a swap was made in the do/while loop.
+//        do {
+//            swapped = false;    // No swap has been made yet so boolean = false.
+//            for (int i = 1; i < n; i++) {   // Iterates over the second element (i=1) until the last element.
+//                if (scoringScheme.compare(archers.get(i - 1), archers.get(i)) > 0) { // Checks if archer at (i-1) has to come before or after (i).
+//                    Archer tempArcher = archers.get(i - 1); // stores the archer at index (i-1) in tempArcher.
+//                    archers.set(i - 1, archers.get(i));     // Sets values of archer at (i-1) at (i).
+//                    archers.set(i, tempArcher);             // Sets the value of the tempArcher (i-1) at (i)
+//                    swapped = true;                         // Now setting the boolean to true after a swap happened.
+//                }
+//            }
+//        } while (swapped); // Continues running as long as swapped is true.
+//        return archers;
+//    } // end of BUBBLE SORT
+
+
+
+        // INSERTION SORT:
+
+        for (int i = 0; i < archers.size(); i++) {  // For loop that runs through all the items in the archers list.
+            Archer currentArcher = archers.get(i);  // Gets the current archer from the loop and stores it as currentArcher.
+            int j = i - 1;                          // Initializing a variable one to the left of the index for comparison.
+            while (j >= 0 && scoringScheme.compare(archers.get(j), currentArcher) > 0) {    // Runs as long as j is not negative and the archer at index J is greater than currentArcher.
+                archers.set(j + 1, archers.get(j)); // If the while loop runs then moves archer from index J to index j+1 (basically i)
+                j--;                                // Auto decrement on j to make sure the previous elemented is also checked.
+            }
+            archers.set(j + 1, currentArcher);      // currentArcher is inserted at the correct position after the loop.
+        }
+        return archers;
+
+     } // end of INSERTION SORT
 
     /**
      * This method uses the Java collections sort algorithm for sorting the archers.
      */
     public static List<Archer> collectionSort(List<Archer> archers, Comparator<Archer> scoringScheme) {
-        // TODO use the sort method of the collection framework
+        Collections.sort(archers, scoringScheme);
         return archers;
     }
 
--- src/main/java/nl/hva/ict/se/ads/Launcher.java
+++ src/main/java/nl/hva/ict/se/ads/Launcher.java
@@ -43,6 +43,33 @@ public class Launcher {
 
         System.out.println();
         System.out.println("------- efficiëntie van sorteer algoritmes ---------");
-        // TODO
+        int [] listSizes = {10000, 20000, 40000}; // Initializes the list sizes. The amount given in the assignment took too long.
+
+        for (int size : listSizes) { // Runs through the given sizes in the listSizes Array.
+            List<Archer> archers = Archer.generateArchers(size); // Using a list to store all Archers with the gigven generateArchers method. Passing size in the loop to give the amount of archers to create.
+
+            long startTime = System.currentTimeMillis(); // Stores the start time in a long.
+            ChampionSelector.basicSort(archers, new Archer.schema1Comparator()); // Calling the sort method. Depending on what is active in the method can be bubble or insertion sort.
+            long endTime = System.currentTimeMillis(); // Stores the end time in a long.
+            long finalRunTime = endTime - startTime; // Calculates the total time spent before the loop finished.
+            System.out.printf("%d: \t Started at: %d ended at: %d total time(ms): %d\n",size, startTime, endTime, finalRunTime);
+
+
+//            Output using INSERTION SORT:
+//            10000: 	 Started at: 1720539910566 ended at: 1720539911718 total time(ms): 1152
+//            20000: 	 Started at: 1720539911740 ended at: 1720539916323 total time(ms): 4583
+//            40000: 	 Started at: 1720539916344 ended at: 1720539934115 total time(ms): 17771
+
+//            Output using BUBBLE SORT:
+//            10000: 	 Started at: 1720539514790 ended at: 1720539524765 total time(ms): 9975
+//            20000: 	 Started at: 1720539524779 ended at: 1720539565052 total time(ms): 40273
+//            40000: 	 Started at: 1720539565073 ended at: 1720539790313 total time(ms): 225240
+
+            /**
+            @CONCLUSION:
+             Both methods prove to be highly ineffective. Insertion & Bubble sort both yield an execution time that roughly increases quadratically.
+             That means time complexity is O(n^2).
+             */
+        }
     }
 }
